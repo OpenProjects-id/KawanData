d8f64119c2e9aff277e2b7a1f16b6cbdee7fa67c
algonacci <algonacci1@gmail.com>
2021-12-07 01:06:52 +0700

    fix: typo on secure_asset CSS

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{ asset('css/main.css') }}" type="text/css">
+    <link rel="stylesheet" href="{{ secure_asset('css/main.css') }}" type="text/css">
 
     <link rel="icon" href="{{ asset('images/icon.png') }}">
 
@@ -24,7 +24,7 @@
     <nav class="navbar navbar-expand-lg navbar-light">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="{{ secure_asset('images/logo.png') }}" alt="">
+                <img src="{{ asset('images/logo.png') }}" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -248,7 +248,7 @@
                 </div>
                 <div class="col-lg-7 col-12">
                     <div class="row">
-                        <div class="col-lg-6 col-12">
+                        <div class="col-lg-6 col-12 py-3">
                             <div class="table-pricing paket-biasa">
                                 <p class="story text-center">
                                     BARU MULAI
@@ -294,7 +294,7 @@
                                 </p>
                             </div>
                         </div>
-                        <div class="col-lg-6 col-12">
+                        <div class="col-lg-6 col-12 py-3">
                             <div class="table-pricing paket-biasa">
                                 <p class="story text-center">
                                     LUMAYAN PAHAM
