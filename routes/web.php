2f7b8f1d4cffc9b426002d6b13ab064cca662f7b
Nurfitra Pujo Santiko <fitrapujo@gmail.com>
2021-12-13 14:25:03 +0700

    feat: add login with facebook handler and router

--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,9 @@ Route::get('sign-in-google', [UserController::class, 'google'])->name('user.logi
 
 Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.google.callback');
 
+Route::get('auth/facebook', [SocialController::class, 'facebookRedirect']);
+
+Route::get('auth/facebook/callback', [SocialController::class, 'loginWithFacebook']);
+
+
 require __DIR__.'/auth.php';
