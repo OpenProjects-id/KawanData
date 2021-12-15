84f583285226f0afb806934627a6683586c5a2b2
algonacci <algonacci1@gmail.com>
2021-12-16 01:47:49 +0700

    Update dashboard.blade.php

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,53 +1,56 @@
 @extends('layouts.app')
-
 @section('content')
-<section class="dashboard my-5">
-    <div class="container">
-        <div class="row text-left">
-            <div class=" col-lg-12 col-12 header-wrap mt-4">
-                <p class="story">
-                    DASHBOARD
-                </p>
-                <h2 class="primary-header ">
-                    Course Saya
-                </h2>
+    <div class="container py-5">
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="card">
+                    <div class="card-header text-center">
+                        Semua Member yang sudah mendaftar Course
+                    </div>
+                    <div class="card-body">
+                        @include('components.alert')
+                        <table class="table">
+                            <thead>
+                                <tr class="text-center">
+                                    <th>User</th>
+                                    <th>Course</th>
+                                    <th>Harga</th>
+                                    <th>Tanggal Daftar</th>
+                                    <th>Status Pembayaran</th>
+                                    <th>Aksi</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($checkouts as $checkout)
+                                    <tr class="text-center">
+                                        <td>{{$checkout->User->name}}</td>
+                                        <td>{{$checkout->Course->title}}</td>
+                                        <td>Rp{{$checkout->Course->price}}</td>
+                                        <td>{{$checkout->created_at->format('M d Y')}}</td>
+                                        <td>
+                                            @if ($checkout->is_paid)
+                                                <span class="badge bg-success">Sudah Bayar</span>
+                                            @else
+                                                <span class="badge bg-warning">Menunggu</span>
+                                            @endif
+                                        </td>
+                                        <td>
+                                            <form action="" method="POST">
+                                                @csrf
+                                                <button class="btn btn-primary btn-sm">Approve</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                    <tr>
+                                        <td colspan="3">No camps registered</td>
+                                    </tr>
+                                @endforelse
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
-        <div class="row my-5">
-            @include('components.alert')
-            <table class="table">
-                <tbody>
-                    @forelse ($checkouts as $checkout)
-                    <tr class="align-middle">
-                        <td width="18%">
-                            <img src="{{asset('images/item_bootcamp.png')}}" height="120" alt="">
-                        </td>
-                        <td>
-                            <p class="mb-2">
-                                <strong>{{$checkout->Course->title}}</strong>
-                            </p>
-                            <p>
-                                {{$checkout->created_at->format('M d, Y')}}
-                            </p>
-                        </td>
-                        <td>
-                            <strong>Rp{{ $checkout->Course->price }}</strong>
-                        </td>
-                        <td class="text-center">
-                            <strong>Sudah Bayar</strong>
-                        </td>
-                        <td class="text-center">
-                            <a href="#" class="btn btn-primary">
-                                Mulai Belajar
-                            </a>
-                        </td>
-                    </tr>
-                    @empty
-                        <h3 class="text-center">No Data</h3>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
     </div>
-</section>
 @endsection
