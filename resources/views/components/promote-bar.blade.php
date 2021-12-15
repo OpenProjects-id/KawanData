f7aa6fbbeeb0801374876b0b54732a40b843f915
algonacci <86970816+algonacci@users.noreply.github.com>
2021-12-15 20:47:04 +0700

    Update promote-bar.blade.php

--- resources/views/components/promote-bar.blade.php
+++ resources/views/components/promote-bar.blade.php
@@ -1,6 +1,6 @@
 <div class="promote-bar container-fluid p-2">
     <p class="text-center text-decoration-none"><a class="text-decoration-none text-dark"
-            href="https://github.com/OpenProjects-id/KawanData" target="_blank">berkontribusi sekarang pada project open source kami
+            href="https://github.com/OpenProjects-id/KawanData" target="_blank">Ingin berkontribusi di KawanData? Cek disini!
             <i class="fab fa-github"></i>
         </a></p>
 </div>
