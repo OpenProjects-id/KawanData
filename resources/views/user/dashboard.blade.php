7a566b5a476507fe805e209a57a56d86c3f5db08
fadila-amin <aminudinfadila@gmail.com>
2021-12-28 10:20:13 +0700

    feat:thumbnail dinamis. get thumbnail on dashboard

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,56 +1,56 @@
 @extends('layouts.app')
 
 @section('content')
-<section class="dashboard my-5">
-    <div class="container">
-        <div class="row text-left">
-            <div class=" col-lg-12 col-12 header-wrap mt-4">
-                <p class="story">
-                    DASHBOARD
-                </p>
-                <h2 class="primary-header ">
-                    Course Saya
-                </h2>
+    <section class="dashboard my-5">
+        <div class="container">
+            <div class="row text-left">
+                <div class=" col-lg-12 col-12 header-wrap mt-4">
+                    <p class="story">
+                        DASHBOARD
+                    </p>
+                    <h2 class="primary-header ">
+                        Course Saya
+                    </h2>
+                </div>
+            </div>
+            <div class="row my-5">
+                @include('components.alert')
+                <table class="table">
+                    <tbody>
+                        @forelse ($checkouts as $checkout)
+                            <tr class="align-middle">
+                                <td width="18%">
+                                    <img src="{{ $checkout->Course->thumbnail }}" height="120" alt="thumbnail course">
+                                </td>
+                                <td>
+                                    <p class="mb-2">
+                                        <strong>{{ $checkout->Course->title }}</strong>
+                                    </p>
+                                    <p>
+                                        {{ $checkout->created_at->format('M d, Y') }}
+                                    </p>
+                                </td>
+                                <td>
+                                    <strong>Rp{{ $checkout->Course->price }}</strong>
+                                </td>
+                                <td class="text-center">
+                                    <strong>Sudah Bayar</strong>
+                                </td>
+                                <td class="text-center">
+                                    <a href="#" class="btn btn-primary">
+                                        Mulai Belajar
+                                    </a>
+                                </td>
+                            </tr>
+                        @empty
+                            <h3 class="text-center">Kamu belum mengikuti Course manapun.
+                                <br>
+                                Silahkan join salah satu Course yang ada di KawanData
+                            </h3>
+                        @endforelse
+                    </tbody>
+                </table>
             </div>
         </div>
-        <div class="row my-5">
-            @include('components.alert')
-            <table class="table">
-                <tbody>
-                    @forelse ($checkouts as $checkout)
-                    <tr class="align-middle">
-                        <td width="18%">
-                            <img src="{{asset('images/item_bootcamp.png')}}" height="120" alt="">
-                        </td>
-                        <td>
-                            <p class="mb-2">
-                                <strong>{{$checkout->Course->title}}</strong>
-                            </p>
-                            <p>
-                                {{$checkout->created_at->format('M d, Y')}}
-                            </p>
-                        </td>
-                        <td>
-                            <strong>Rp{{ $checkout->Course->price }}</strong>
-                        </td>
-                        <td class="text-center">
-                            <strong>Sudah Bayar</strong>
-                        </td>
-                        <td class="text-center">
-                            <a href="#" class="btn btn-primary">
-                                Mulai Belajar
-                            </a>
-                        </td>
-                    </tr>
-                    @empty
-                        <h3 class="text-center">Kamu belum mengikuti Course manapun.
-                            <br>
-                            Silahkan join salah satu Course yang ada di KawanData
-                        </h3>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-    </div>
-</section>
+    </section>
 @endsection
